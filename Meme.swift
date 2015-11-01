863127ab024a2042e2b35afc9996b312a3510708
bkellytech <bkellytech@yahoo.com>
2015-11-01 11:51:37 -0500

    adding model

---
+++ Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe Model class for storing Representing a "Meme"
+//
+//  Created by admin on 10/28/15.
+//  Copyright © 2015 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme{
+    var topTextField: String!
+    var bottomTextField: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
